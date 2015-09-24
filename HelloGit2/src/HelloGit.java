cfcdfbcd5d41ee2cc4982b5c6383e612872e5e4d
Michael Kolta <mike_kolta@yahoo.com>
2015-09-24 09:43:53 -0700

    practicing git

--- HelloGit2/src/HelloGit.java
+++ HelloGit2/src/HelloGit.java
@@ -2,6 +2,6 @@
 public class HelloGit {
 public static void main(String args[])
 {
-	System.out.println("Hello Git! why can't I save hello");
+	System.out.println("Hello Git! practicing Git");
 }
 }
